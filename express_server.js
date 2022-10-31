bc3b8dfb4e6937b50d57da34666ea793a985fdbc
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-31 14:22:33 -0700

    Adds app.get with res.render for file in views directory

--- express_server.js
+++ express_server.js
@@ -18,6 +18,9 @@ app.get('/urls.json', (req, res) => {
 app.get('/helloWorld', (req, res) => {
   res.send("<html><body><h1>Hello World!</h1></body></html>");
 })
+app.get('/hipsum', (req, res) => {
+  res.render('hipsum');
+})
 
 app.listen(PORT, () => {
   console.log(`Express server up: listening on port ${PORT}`);

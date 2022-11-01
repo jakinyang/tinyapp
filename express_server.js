e77f92e2c01ed7383796157f4b359bdb427aa6b9
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-31 18:37:33 -0700

    Adds templateVars and res.render for second route param in url

--- express_server.js
+++ express_server.js
@@ -35,8 +35,8 @@ app.get('/urls', (req, res) => {
   console.log("Client request for /urls");
 })
 app.get('/urls:id', (req, res) => {
-  const templateVars = {urls: urlDatabase};
-  res.render('urls_index', templateVars);
+  const templateVars = { id: req.params.id, longURL: urlDatabase[id]};
+  res.render('urls_show', (templateVars));
   console.log("Request Method: ", req.method);
   console.log("Request URL: ", req.url);
   console.log("Client request for /urls");

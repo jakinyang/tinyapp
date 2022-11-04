a36208d8cba4a3de240fe94e5192a200bcfdf438
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-11-03 17:22:34 -0700

    Moves userRetrieverEmail to own file/module

---
+++ helpers.js
@@ -0,0 +1,12 @@
+const userRetrieverEmail = (database, targetEmail) => {
+  for (let subDatabase in database) {
+    for (let id in database[subDatabase]) {
+      if (database[subDatabase]['email'] === targetEmail) {
+        return database[subDatabase][id];
+      }
+    }
+  }
+  return false;
+}
+
+module.exports = { userRetrieverEmail };
